65b1bff7e938c713490e0c612acb04956baccc41
Marian Domanski <mdomanski@usgs.gov>
2017-08-30 13:30:05 -0500

    Modified main.py

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
+import pdb
 import sys
 
 from PyQt5.QtWidgets import QApplication
 
 from gui.gui import WriteFlowFileMainWindow
 
-app = QApplication(sys.argv)
-main_window = WriteFlowFileMainWindow()
-main_window.show()
-app.exec_()
+if __name__ == "__main__":
+
+    app = QApplication(sys.argv)
+    main_window = WriteFlowFileMainWindow()
+    main_window.show()
+    sys.exit(app.exec_())
