d412f441c40bb488ddbc1a271b71071906e80c56
Marian Domanski <mdomanski@usgs.gov>
2017-09-28 16:13:22 -0500

    Renamed from main.pyw

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-import pdb
 import sys
 
 from PyQt5.QtWidgets import QApplication
 
-from gui.gui import WriteFlowFileMainWindow
+# from gui.gui import WriteFlowFileMainWindow
+from gui.feqrasmapper import FEQRASMapperMainWindow
 
 if __name__ == "__main__":
 
     app = QApplication(sys.argv)
-    main_window = WriteFlowFileMainWindow()
+    main_window = FEQRASMapperMainWindow()
     main_window.show()
     sys.exit(app.exec_())
