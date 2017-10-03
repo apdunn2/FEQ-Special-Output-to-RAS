b6d14304cc76bbda2fe05e680c1c8a148a60a612
Marian Domanski <mdomanski@usgs.gov>
2017-10-03 11:52:05 -0500

    Renamed Results to ResultsFile

--- feqtoras.py
+++ feqtoras.py
@@ -3,6 +3,7 @@ import copy
 import pandas as pd
 
 import feq
+from hydraulic import HydraulicTimeSeries
 import ras
 
 
@@ -13,15 +14,15 @@ class FEQToRAS:
         self._feq_special_output = feq_special_output
 
     @staticmethod
-    def _adjust_node_elevation(node_table, elevation_df):
+    def _adjust_node_elevation(node_table, hydraulic_data):
 
         if 'Elev Adj' in node_table.keys():
-            for river, reach, node in elevation_df.columns:
+            for river, reach, node, _ in hydraulic_data.columns:
                 river_index = node_table['River'] == river
                 reach_index = node_table['Reach'] == reach
                 node_index = node_table['Node'] == node
                 elevation_adjustment = node_table.loc[river_index & reach_index & node_index, 'Elev Adj'].values[0]
-                elevation_df.loc[:, (river, reach, node)] += elevation_adjustment
+                hydraulic_data.loc[:, (river, reach, node, 'Elev')] += elevation_adjustment
 
     @staticmethod
     def _combine_dataframes(args):
@@ -29,17 +30,16 @@ class FEQToRAS:
 
     @staticmethod
     def _convert_node_to_cross_section(node_table, constituent_df):
-        constituent_df.columns.names = ['river', 'reach', 'cross section']
 
         node_reference_table = node_table.copy(deep=True)
         node_reference_table.set_index('River', inplace=True)
         node_reference_table.set_index('Reach', inplace=True, append=True)
         node_reference_table.set_index('Node', inplace=True, append=True)
 
-        list_of_tuples = [(river, reach, node_reference_table.loc[river, reach, node][0])
-                          for river, reach, node in constituent_df.columns.values]
+        list_of_tuples = [(river, reach, node_reference_table.loc[river, reach, node][0], value)
+                          for river, reach, node, value in constituent_df.columns.values]
 
-        new_columns = pd.MultiIndex.from_tuples(list_of_tuples)
+        new_columns = pd.MultiIndex.from_tuples(list_of_tuples, names=['river', 'reach', 'cross section', 'value'])
 
         constituent_df.columns = new_columns
 
@@ -59,27 +59,31 @@ class FEQToRAS:
         node_table['River & Reach'] = node_table['River'] + ',' + node_table['Reach']
         return node_table
 
+    def get_ras_time_series(self):
+        feq_data = self._feq_special_output.get_data()
+        self._adjust_node_elevation(self._node_table, feq_data)
+        self._convert_node_to_cross_section(self._node_table, feq_data)
+        return HydraulicTimeSeries(feq_data)
+
     def get_ras_elevation(self, start_date=None, end_date=None, number_of_days=None, time_step=None):
 
-        elevation_df = self._feq_special_output.get_constituent('Elev',
-                                                                start_date=start_date,
-                                                                end_date=end_date,
-                                                                number_of_days=number_of_days,
-                                                                time_step=time_step)
-
-        self._adjust_node_elevation(self._node_table, elevation_df)
-        self._convert_node_to_cross_section(self._node_table, elevation_df)
+        ras_time_series = self.get_ras_time_series()
+        elevation_df = ras_time_series.get_constituent('Elev',
+                                                       start_date=start_date,
+                                                       end_date=end_date,
+                                                       number_of_days=number_of_days,
+                                                       time_step=time_step)
 
         return elevation_df
 
     def get_ras_flow(self, start_date=None, end_date=None, number_of_days=None, time_step=None):
 
-        flow_df = self._feq_special_output.get_constituent('Flow',
-                                                           start_date=start_date,
-                                                           end_date=end_date,
-                                                           number_of_days=number_of_days,
-                                                           time_step=time_step)
-        self._convert_node_to_cross_section(self._node_table, flow_df)
+        ras_time_series = self.get_ras_time_series()
+        flow_df = ras_time_series.get_constituent('Flow',
+                                                  start_date=start_date,
+                                                  end_date=end_date,
+                                                  number_of_days=number_of_days,
+                                                  time_step=time_step)
 
         return flow_df
 
@@ -126,6 +130,8 @@ class FEQRASMapper:
 
         self._special_output = feq_special_output
 
+        return feq_special_output
+
     def write_feq_results_to_ras(self):
 
         node_file_path = self._config['Node table']
@@ -138,7 +144,7 @@ class FEQRASMapper:
         ras_flow_df = feq_to_raser.get_ras_flow(number_of_days=number_of_days, time_step=time_step)
 
         h5_file_name = self._ras_mapper.get_results_file(self._export_options.plan_name)
-        ras_results = ras.Results(h5_file_name)
+        ras_results = ras.ResultsFile(h5_file_name)
 
         ras_results.write_data(ras_elevation_df, ras_flow_df)
 
